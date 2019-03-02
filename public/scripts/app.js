75990f69160d76f34b737b7602868925e2e10fad
amilford87 <amilford87@gmail.com>
2019-03-02 16:20:31 +0000

    homepage changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -75,7 +75,7 @@ function updateTotal(total){
   }
 }
 
-console.log($("#total-drinks"));
+// console.log($("#total-drinks"));
 
 function totalPriceSum( obj ) {
   var sum = 0;
@@ -91,69 +91,69 @@ function updateTotalPrice(total){
   if (total < 0){
     totalPrice = 0;
   }
-
+  
   else{
     $("#footer-total").text(`$${total} Total`);
   }
 }
 
-  $(".increase").on("click", function (event)
-  {
-
-    let price = event.target.previousSibling.previousSibling.innerHTML;
-    let priceNumber = Number(price.replace(/[^0-9.-]+/g,""));
+$(".increase").on("click", function (event)
+{
+  
+  let price = event.target.previousSibling.previousSibling.innerHTML;
+  let priceNumber = Number(price.replace(/[^0-9.-]+/g,""));
     
-    let itemId = event.target.nextSibling.nextSibling.id;
-
-    
-    let counter = event.target.nextSibling.nextSibling.innerHTML;
-    console.log(event);
-    totalCounter++;
-    counter++;
-    console.log(event.target.nextSibling.nextSibling.nextSibling.nextSibling);
-
-    if (counter > 0) {
-      $(event.target.nextSibling.nextSibling).removeClass("hide");
-      $(event.target.nextSibling.nextSibling.nextSibling.nextSibling).addClass("show-button");
-    }
-    if (counter > 9){
-      $(event.target.nextSibling.nextSibling).addClass("smaller");
-    }
-    else {
-      $(event.target.nextSibling.nextSibling).removeClass("smaller");
-    }
-    $(event.target.nextSibling.nextSibling).text(counter);
-
-      if (!Object.keys(cart).includes(itemId)){
-        cart[itemId] = 1;
-      }
-      else {
-        cart[itemId]++;
-      }
-
-      totalPrice += priceNumber;
-    console.log(cart);
+  let itemId = event.target.nextSibling.nextSibling.id;
+  
+  
+  let counter = event.target.nextSibling.nextSibling.innerHTML;
+  // console.log(event);
+  totalCounter++;
+  counter++;
+  // console.log(event.target.nextSibling.nextSibling.nextSibling.nextSibling);
+  
+  if (counter > 0) {
+    $(event.target.nextSibling.nextSibling).removeClass("hide");
+    $(event.target.nextSibling.nextSibling.nextSibling.nextSibling).addClass("show-button");
+  }
+  if (counter > 9){
+    $(event.target.nextSibling.nextSibling).addClass("smaller");
+  }
+  else {
+    $(event.target.nextSibling.nextSibling).removeClass("smaller");
+  }
+  $(event.target.nextSibling.nextSibling).text(counter);
+  
+  if (!Object.keys(cart).includes(itemId)){
+    cart[itemId] = 1;
+  }
+  else {
+    cart[itemId]++;
+  }
+  
+  totalPrice += priceNumber;
+    // console.log(cart);
     updateTotal(totalCounter);
     updateTotalPrice(totalPrice);
+    
+  });
   
-});
-
-$(".decrease").on("click", function (event)
-{
-  let counter = event.target.previousSibling.previousSibling.innerHTML;
-  let price = event.target.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.innerHTML;
-  let priceNumber = Number(price.replace(/[^0-9.-]+/g,""));
-  let itemId = event.target.previousSibling.previousSibling.id;
-  
-  if (cart[itemId] < 1 || !Object.keys(cart).includes(itemId)){
-    cart[itemId] = 0;
-  }
-  totalCounter--;
-  counter--;
-  cart[itemId]--;
-  
-  if (counter < 0){
-    counter = 0;
+  $(".decrease").on("click", function (event)
+  {
+    let counter = event.target.previousSibling.previousSibling.innerHTML;
+    let price = event.target.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.innerHTML;
+    let priceNumber = Number(price.replace(/[^0-9.-]+/g,""));
+    let itemId = event.target.previousSibling.previousSibling.id;
+    
+    if (cart[itemId] < 1 || !Object.keys(cart).includes(itemId)){
+      cart[itemId] = 0;
+    }
+    totalCounter--;
+    counter--;
+    cart[itemId]--;
+    
+    if (counter < 0){
+      counter = 0;
     cart[itemId] = 0;
   }
   else if (counter < 10){
@@ -163,12 +163,19 @@ $(".decrease").on("click", function (event)
     $(event.target.previousSibling.previousSibling).addClass("hide");
     $(this).removeClass("show-button");
   }
-
+  
   $(event.target.previousSibling.previousSibling).text(counter);
   totalPrice = totalPrice - priceNumber;
-    updateTotalPrice(totalPrice);
-    updateTotal(totalCounter);
-  console.log(cart);
+  updateTotalPrice(totalPrice);
+  updateTotal(totalCounter);
+  // console.log(cart);
 });
-//  5adec31f2508629f6f4e965254b4066cae5f012b
+
+
+$("#increase-myo").on("click", function (event)
+{
+  var $customMenu = $(event.target.parentElement.parentElement.parentElement.parentElement.parentElement.children[2]);
+  $customMenu.addClass("customize-menu-show");
+});
+
 });
--- public/scripts/checkout.js
+++ public/scripts/checkout.js
@@ -1,18 +1,28 @@
 var totalIngredientPrice = 0;
+var checkedItems = [];
+
 
 $(document).ready(function() {
+    function arrayRemove(arr, value) {
 
+        return arr.filter(function(ele){
+            return ele != value;
+        });
+     
+     }
     
     $('.checkbox').on("click", function(event) {
         let price = event.target.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.innerHTML;
         let priceNumber = Number(price.replace(/[^0-9.-]+/g,""));
         if (event.target.checked === true){
             totalIngredientPrice = totalIngredientPrice + priceNumber;
+            checkedItems.push(event.target.id);
         } else{
             totalIngredientPrice = totalIngredientPrice - priceNumber;
+            checkedItems = arrayRemove(checkedItems, event.target.id);
         }
         console.log(totalIngredientPrice);
-        
+        console.log(checkedItems);
     });
 });
 
